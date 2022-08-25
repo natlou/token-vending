07dc96f0e67c6e5b86e733cc0da7d49e3f5b5073
natlou <natloudev@gmail.com>
2022-08-25 14:24:34 -0700

    emit event after buyToken() call

--- packages/hardhat-ts/contracts/Vendor.sol
+++ packages/hardhat-ts/contracts/Vendor.sol
@@ -15,8 +15,11 @@ contract Vendor {
 
   uint256 public constant tokensPerEth = 100;
 
+  event BuyTokens(address buyer, uint256 amountOfEth, uint256 amountOfTokens);
+
   function buyTokens() public payable {
     yourToken.transfer(msg.sender, msg.value * tokensPerEth);
+    emit BuyTokens(msg.sender, msg.value, msg.value * tokensPerEth);
   }
 
   // ToDo: create a withdraw() function that lets the owner withdraw ETH
